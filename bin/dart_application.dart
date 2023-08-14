7b8e0813b07d5648549c3b67d103b03859cced2d
FAdiGabriele <fabioa.digabriele@gmail.com>
2023-08-14 22:46:24 +0200

    add main

---
+++ bin/dart_application.dart
@@ -0,0 +1,27 @@
+import 'package:dart_project/dart_function_library.dart' as dart_application;
+import 'Persona.dart';
+
+void main(List<String> arguments) {
+  print("Richiesta 1");
+  dart_application.printTriangle(6);
+
+  print("\nRichiesta 2");
+  dart_application.printIsPalindrome("ingegni");
+
+  dart_application.printIsPalindrome("gatto");
+
+  dart_application.printIsPalindrome("Anna");
+
+  print("\nRichiesta 3");
+  dart_application.printRectagleArea(4, 6);
+
+  print("\nRichiesta 4");
+  dart_application.printSommaProdotto(2, 3);
+
+  dart_application.printSommaProdotto(2, 2);
+
+  print("\nRichiesta 6");
+
+  Persona("Mario", "Rossi", "mario.rossi@email.it", "Persona1_2023", "01/01/1990");
+  Persona("Luigi", "Bianchi", "luigi.bianchi@email.it", "Persona2_2023", "01/06/1980");
+}
