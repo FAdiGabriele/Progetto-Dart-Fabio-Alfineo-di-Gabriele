a21ea10378d5c746d3114223374608d95e52e986
FAdiGabriele <fabioa.digabriele@gmail.com>
2023-08-15 02:38:06 +0200

    core: add unit test

--- test/dart_application_test.dart
+++ test/dart_application_test.dart
@@ -13,4 +13,48 @@ void main() {
   test('isPalindrom false', () {
     expect(isPalindrome("gatto"), false);
   });
+
+  test('getRectangleArea works', () {
+    expect(getRectangleArea(3, 4), 12);
+  });
+
+  test("getSommaProdotto different values", () {
+    expect(getSommaProdotto(3, 4), 7);
+  });
+
+  test("getSommaProdotto same values", () {
+    expect(getSommaProdotto(3, 3), 18);
+  });
+
+  test("getExameGrade invalid praticExamGrade", () {
+    expect(getExameGrade(-3, 4), "Prova Pratica non valida");
+  });
+
+  test("getExameGrade invalid praticExamGrade 2", () {
+    expect(getExameGrade(25, 4), "Prova Pratica non valida");
+  });
+
+  test("getExameGrade invalid writtenExamGrade", () {
+    expect(getExameGrade(21, -9), "Prova Scritta non valida");
+  });
+
+  test("getExameGrade invalid writtenExamGrade 2", () {
+    expect(getExameGrade(21, 9), "Prova Scritta non valida");
+  });
+
+  test("getExameGrade low grade", () {
+    expect(getExameGrade(10, 4), "Bocciato");
+  });
+
+  test("getExameGrade max grade", () {
+    expect(getExameGrade(24, 8), "30 e lode");
+  });
+
+  test("getExameGrade normal grade", () {
+    expect(getExameGrade(21, 4), "25");
+  });
+
+  test("getAverage works", () {
+    expect(getAverage([2, 4]), 3);
+  });
 }
