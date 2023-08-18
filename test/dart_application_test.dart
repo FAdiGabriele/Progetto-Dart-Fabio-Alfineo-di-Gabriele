bb74f8927220e9cb0d06e3eabba82ad641a89fad
FAdiGabriele <fabioa.digabriele@gmail.com>
2023-08-18 14:20:39 +0200

    core: add unit tests for classes

--- test/dart_application_test.dart
+++ test/dart_application_test.dart
@@ -1,5 +1,7 @@
 import 'package:dart_project/dart_function_library.dart';
 import 'package:test/test.dart';
+import 'package:dart_project/Universita.dart';
+import 'package:dart_project/Persona.dart' as persona_request;
 
 void main() {
   test('isPalindrom true', () {
@@ -30,15 +32,15 @@ void main() {
     expect(isPraticExamGradeNotValid(-3), true);
   });
 
-    test('isPraticExamGradeNotValid false', () {
+  test('isPraticExamGradeNotValid false', () {
     expect(isPraticExamGradeNotValid(18), false);
   });
 
-    test('isWrittenExamGradeNotValid true', () {
+  test('isWrittenExamGradeNotValid true', () {
     expect(isPraticExamGradeNotValid(-8), true);
   });
 
-    test('isWrittenExamGradeNotValid false', () {
+  test('isWrittenExamGradeNotValid false', () {
     expect(isPraticExamGradeNotValid(1), false);
   });
 
@@ -71,6 +73,41 @@ void main() {
   });
 
   test("getAverage works", () {
-    expect(getAverage([2, 4]), 3);
+    expect(getAverage([2, 4, 6, 8]), 5);
+  });
+
+  test("create persona works", () {
+    persona_request.Persona persona = persona_request.Persona(
+        "nome_persona",
+        "cognome_persona",
+        "email_persona",
+        "passowrd_persona",
+        "data_di_nascita");
+
+    expect(persona.nome, "nome_persona");
+    expect(persona.cognome, "cognome_persona");
+    expect(persona.email, "email_persona");
+    expect(persona.password, "passowrd_persona");
+    expect(persona.dataDiNascita, "data_di_nascita");
+  });
+
+  test("create docente works", () {
+    Docente docente =
+        Docente("nome_docente", "cognome_docente", "codice_docente", 38);
+
+    expect(docente.getNome(), "nome_docente");
+    expect(docente.getCognome(), "cognome_docente");
+    expect(docente.getCodice(), "codice_docente");
+    expect(docente.getEta(), 38);
+  });
+
+  test("create studente works", () {
+    Studente studente =
+        Studente("nome_studente", "cognome_studente", "codice_studente", 20);
+
+    expect(studente.getNome(), "nome_studente");
+    expect(studente.getCognome(), "cognome_studente");
+    expect(studente.getCodice(), "codice_studente");
+    expect(studente.getEta(), 20);
   });
 }
